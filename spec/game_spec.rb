846a2d7cac6b32f0fc770a54404ee9ee4fc038b1
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-26 15:30:23 +0000

    Changed TicTacToeGrid class name to Board for clarity

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,8 @@ require 'game'
 describe Game do
   let(:player_1) { double :player_1, name: "player_1"}
   let(:player_2) { double :player_2 }
-  let(:grid) { double :grid }
-  subject(:game) { described_class.new(player_1, player_2, grid) }
+  let(:board) { double :board }
+  subject(:game) { described_class.new(player_1, player_2, board) }
 
   it "has player_1" do
     expect(game.player_1).to eq player_1
@@ -12,46 +12,46 @@ describe Game do
 
   it "players take turns until game is over" do
     params = {row: :a, column: 0, value: :x}
-    allow(grid).to receive(:field_taken?).with(params) { false }
-    allow(grid).to receive(:change_field).with(params)
-    allow(grid).to receive(:win?) { false }
-    allow(grid).to receive(:full?) { false }
+    allow(board).to receive(:field_taken?).with(params) { false }
+    allow(board).to receive(:change_field).with(params)
+    allow(board).to receive(:win?) { false }
+    allow(board).to receive(:full?) { false }
     game.play(params)
     expect(game.active_player).to eq player_2
   end
 
   it "lets active player take a field" do
     params = {row: :a, column: 0, value: :x}
-    allow(grid).to receive(:field_taken?).with(params) { false }
-    expect(grid).to receive(:change_field).with(params)
-    allow(grid).to receive(:win?) { false }
-    allow(grid).to receive(:full?) { false }
+    allow(board).to receive(:field_taken?).with(params) { false }
+    expect(board).to receive(:change_field).with(params)
+    allow(board).to receive(:win?) { false }
+    allow(board).to receive(:full?) { false }
     game.play(params)
   end
 
   it "doesn't allow player to take an already taken field" do
     params = {row: :a, column: 0, value: :x}
-    allow(grid).to receive(:field_taken?).with(params) { true }
-    expect(grid).not_to receive(:change_field)
-    allow(grid).to receive(:win?) { false }
-    allow(grid).to receive(:full?) { false }
+    allow(board).to receive(:field_taken?).with(params) { true }
+    expect(board).not_to receive(:change_field)
+    allow(board).to receive(:win?) { false }
+    allow(board).to receive(:full?) { false }
     game.play(params)
   end
 
   it "ends when active player wins" do
     params = {row: :a, column: 0, value: :x}
-    allow(grid).to receive(:field_taken?).with(params) { false }
-    allow(grid).to receive(:change_field).with(params)
-    allow(grid).to receive(:win?) { true }
+    allow(board).to receive(:field_taken?).with(params) { false }
+    allow(board).to receive(:change_field).with(params)
+    allow(board).to receive(:win?) { true }
     expect { game.play(params) }.to raise_exception("Game over! player_1 wins!")
   end
 
-  it "ends when grid full" do
+  it "ends when board full" do
     params = {row: :a, column: 0, value: :x}
-    allow(grid).to receive(:field_taken?).with(params) { false }
-    allow(grid).to receive(:change_field).with(params)
-    allow(grid).to receive(:win?) { false }
-    allow(grid).to receive(:full?) { true }
+    allow(board).to receive(:field_taken?).with(params) { false }
+    allow(board).to receive(:change_field).with(params)
+    allow(board).to receive(:win?) { false }
+    allow(board).to receive(:full?) { true }
     expect { game.play(params) }.to raise_exception("Game over, it's a draw!")
   end
 
