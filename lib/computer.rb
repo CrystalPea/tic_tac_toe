45114c6a10b0def4e476222c813105ebf11872e5
Pea M. Tyczynska <CrystalPea@users.noreply.github.com>
2017-02-16 19:38:53 +0000

    Refactored Computer class' method make_move for SRP and readability

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,6 +1,7 @@
 require_relative 'player'
 class Computer < Player
   GOOD_FIRST_MOVES = [{row: :a, column: 0}, {row: :a, column: 2}, {row: :b, column: 1}, {row: :c, column: 0}, {row: :c, column: 2}]
+  ROWS = [:a, :b, :c]
   @my_value = nil
   @opponent = nil
   attr_reader :my_value, :opponent
@@ -24,12 +25,28 @@ class Computer < Player
   attr_writer :my_value, :opponent
   
   def choose_best_move(board)
-    rows = [:a, :b, :c]
-    #danger_in_row?
-    board.grid.each { |row| return {row: rows[board.grid.index(row)], column: row.index(nil)} if row.count(opponent) == 2 }
-    #danger_in_column?
+    if row = danger_in_row?(board)
+      return {row: ROWS[board.grid.index(row)], column: row.index(nil)}
+    end
+    sideways = flip_board(board)
+    if row = danger_in_column?(sideways)
+      return {row: ROWS[row.index(nil)], column: sideways.index(row) }
+    end
+  end
+  
+  def danger_in_row?(board)
+    board.grid.each { |row| return row  if row.count(opponent) == 2 }
+    false
+  end
+
+  def danger_in_column?(sideways)
+    sideways.each {|row| return row if row.count(opponent) == 2 }
+    false
+  end
+    
+  def flip_board(board)
     sideways = [[], [], []]
     board.grid.each {|row| sideways[0] << row[0]; sideways[1] << row[1]; sideways[2] << row[2] }
-    sideways.each {|row| return {row: rows[row.index(nil)], column: sideways.index(row) } if row.count(opponent) == 2 }
+    sideways
   end
 end
