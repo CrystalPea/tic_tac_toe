0fb2a660404306b41b1171f1c837fdde8ec9a096
Pea M. Tyczynska <CrystalPea@users.noreply.github.com>
2017-02-16 13:30:04 +0000

    Computer can prevent opponent from having a horizontal win and computer knows what value it plays

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,13 +1,30 @@
 require_relative 'player'
 class Computer < Player
   GOOD_FIRST_MOVES = [{row: :a, column: 0}, {row: :a, column: 2}, {row: :b, column: 1}, {row: :c, column: 0}, {row: :c, column: 2}]
-  
+  @my_value = nil
+  @opponent = nil
+  attr_reader :my_value, :opponent
+ 
   def board_empty?(board)
     board.empty?
   end
 
-  def make_move
-    play(GOOD_FIRST_MOVES.sample)
+
+  def make_move(board)
+    if board_empty?(board)
+      self.my_value, self.opponent = :x, :o
+      play(GOOD_FIRST_MOVES.sample)
+    else
+     self.my_value, self.opponent = :o, :x if board.unique_elements_number == 2
+     choose_best_move(board) 
+    end
   end
 
+  private
+  attr_writer :my_value, :opponent
+  
+  def choose_best_move(board)
+    rows = [:a, :b, :c]
+    board.grid.each { |row| return {row: rows[board.grid.index(row)], column: row.index(nil)} if row.count(opponent) == 2 }
+  end
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -13,18 +13,46 @@ describe Computer do
       expect(computer.play(hash)).to eq({row: :a, column: 0})
     end
   end
-  it "checks if board is empty" do
-    allow(board).to receive(:empty?) { true }
-    expect(computer.board_empty?(board)).to eq true
+  describe "#empty?" do
+    it "checks if board is empty" do
+      allow(board).to receive(:empty?) { true }
+      expect(computer.board_empty?(board)).to eq true
+    end
+   it "knows if the board not empty" do
+     allow(board).to receive(:empty?) { false }
+     expect(computer.board_empty?(board)).to eq false
+    end
   end
-  it "knows if the board not empty" do
-    allow(board).to receive(:empty?) { false }
-    expect(computer.board_empty?(board)).to eq false
-  end
-  it "makes a move in one of the corners or centre if board empty" do
-    allow(board).to receive(:empty?) { true }
-    good_first_moves = [{row: :a, column: 0}, {row: :a, column: 2}, {row: :b, column: 1}, {row: :c, column: 0}, {row: :c, column: 2}]
-    expect(good_first_moves).to include(computer.make_move) 
+  describe "#make_move" do
+    context "board empty" do
+      it "makes a move in one of the corners or centre if board empty" do
+        allow(board).to receive(:empty?) { true }
+        good_first_moves = [ {row: :a, column: 0}, {row: :a, column: 2}, {row: :b, column: 1}, {row: :c, column: 0}, {row: :c, column: 2} ]
+        expect(good_first_moves).to include(computer.make_move(board)) 
+      end
+      it "knows that it plays :x when it gets first move" do
+        allow(board).to receive(:empty?) { true }
+        computer.make_move(board)
+        expect(computer.my_value).to eq :x
+      end
+    end
+    
+    context "board not empty" do
+      it "prevents oppopent from getting a winning row" do
+        allow(board).to receive(:empty?) { false }
+        allow(board).to receive(:unique_elements_number) { 2 }
+        allow(board).to receive(:grid) { [[:x, nil, nil], [:x, :x, nil], [nil, nil, nil]] }
+        expect(computer.make_move(board)).to eq({row: :b, column: 2})
+      end
+
+      it "knows that it plays :o if opponent moved first" do
+        allow(board).to receive(:empty?) { false }
+        allow(board).to receive(:unique_elements_number) { 2 }
+        allow(board).to receive(:grid) { [[:x, nil, nil], [nil, nil, nil], [nil, nil, nil]] }
+        computer.make_move(board)
+        expect(computer.my_value).to eq :o
+      end
+    end
   end
 end
 
